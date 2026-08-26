2d6380acd971131ca091587441d3d52616deb315
kerollen <kerollen.soares@gmail.com>
2026-08-26 11:21:39 -0300

    Refactor code structure for improved readability and maintainability

--- entry-sheets.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app-sheets-source.jsx";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
